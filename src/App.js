62a5c457d456cf21feabbaec55ab7751fa9b27a4
Luis Suarez <suarezluis@gmail.com>
2019-06-29 10:44:11 -0500

    CheatSheet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, signInOut } from "./actions";
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(2))}>+</button>
+      <button onClick={() => dispatch(decrement(1))}>- </button>
+      {isLogged ? (
+        <h3>Valuable Information only available for logged in users</h3>
+      ) : (
+        <h3>User Not Logged In</h3>
+      )}
+
+      <button onClick={() => dispatch(signInOut())}>
+        {isLogged ? "Log Out" : "Log In"}
+      </button>
     </div>
   );
 }
